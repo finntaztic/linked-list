e2d2eb14d94c8f0db6e44975993e08221e157d9c
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-08-13 08:26:40 +0800

    Append ok

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
 
 class Node {
   //this is to create a new node
-  constructor(value = null, nextNode = null){
+  constructor(value = null, next = null){
     this.value = value;
-    this.nextNode = nextNode;
+    this.next = next;
   }
 }
 
-
 class LinkedList {
-
   //add value to the end of the list
+  constructor(){
+    this.head=null
+  }
+
   append(value){
-     const newNode = new Node(value);
-        newNode.next = head;
-        return newNode
-  }
+    let newNode = new Node(value)
+    if(!this.head){
+      this.head = newNode;
+      return;
+    }
 
-  //add value to the head of the list
+    let current = this.head
+    while (current.next){
+      current = current.next
+      }
+      current.next = newNode;
+    }
+
+  //adds a new node containing value to the start of the list
   prepend(value){
-    const newNode = new Node(value);
-        newNode.next = head;
-        return newNode
+    
   }
-
-  size(){
-
-  }
-
-  head(){
-
-  }
-
-  tail(){
-
-  }
-
-  at(index){
-
-  }
-
-  pop(){
-
-  }
-
-  contains(value){
-
-  }
-
-  find(value){
-
-  }
-
   toString(){
-
+    let current = this.head
+    let result = ""
+    while(current){
+      result += current.value + '->'
+      current = current.next
+    }
+    return result + 'null'
   }
+
+
+  // add value to the head of the list
+  // prepend(value){
+  //   const newNode = new Node(value);
+  //       newNode.next = head;
+  //       return newNode
+  // }
+
+  // size(){
+
+  // }
+
+  // head(){
+
+  // }
+
+  // tail(){
+
+  // }
+
+  // at(index){
+
+  // }
+
+  // pop(){
+
+  // }
+
+  // contains(value){
+
+  // }
+
+  // find(value){
+
+  // }
+
+  // toString(){
+
+  // }
 }
 
 
